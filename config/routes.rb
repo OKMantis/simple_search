34a9b033613a34cefb8d70998e2d8e435479d288
Owen Moss <owenkmoss@gmail.com>
2019-09-22 19:47:28 +0200

    fix the routes to point to controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'cocktails/index'
-  root to: 'cocktails/index'
+  root to: 'cocktails#index'
+  resources :cocktails
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
