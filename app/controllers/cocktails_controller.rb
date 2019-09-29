fba7e4d1518bdabdc1dfff7a7ef69ba9c3c517de
Owen Moss <owenkmoss@gmail.com>
2019-09-29 20:20:03 +0200

    add search method to controller

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -2,9 +2,13 @@ class CocktailsController < ApplicationController
   def index
     if params["search"]
       @filter = params["search"]["flavors"].concat(params["search"]["strengths"]).flatten.reject(&:blank?)
-      @cocktails = @filter.empty? ? Cocktail.all : Cocktail.all.tagged_with(@filter, any: true)
+      @cocktails = Cocktail.all.global_search("#{@filter}")
     else
       @cocktails = Cocktail.all
+    end
+    respond_to do |format|
+      format.html
+      format.js
     end
   end
 end
